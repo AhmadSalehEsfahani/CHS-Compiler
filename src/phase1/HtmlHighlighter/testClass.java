029ac28ce5730f21e7be6d2f3d63c19cbe6b5fa6
Mohammad Mahdi Chizari <chizari.mm@gmail.com>
2021-04-11 23:05:43 +0430

    Add HtmlHighlighter

---
+++ src/phase1/HtmlHighlighter/testClass.java
@@ -0,0 +1,16 @@
+public class testClass {
+    public static void main(String[] args) {
+        HtmlHighlighter htmlHighlighter = new HtmlHighlighter();
+        htmlHighlighter.comments("myComment-yellow");
+        htmlHighlighter.integerNumbers("orange");
+        htmlHighlighter.newLine();
+        htmlHighlighter.newLine();
+        htmlHighlighter.reservedKeyWords("txtBlueBold");
+        htmlHighlighter.undefinedToken("red-error");
+        htmlHighlighter.space();
+        htmlHighlighter.space();
+        htmlHighlighter.reservedKeyWords("txtBlueBold");
+        htmlHighlighter.specialCharacters("greenItalicTxt");
+        htmlHighlighter.endFile();
+    }
+}
