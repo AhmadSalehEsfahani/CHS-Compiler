c2f77b6d724e82a3f7f459d1a9b07abcc8c74645
Ahmad Saleh <asalehesfahani@gmail.com>
2021-04-11 20:44:52 +0430

    first commit, we add template of phase 1

---
+++ src/phase1/Main.java
@@ -0,0 +1,20 @@
+package phase1;
+
+import java.io.FileReader;
+import java.io.IOException; 
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        
+        final String fileAddress = "code.txt";
+        Lexer lexer = new Lexer(new FileReader(fileAddress));
+
+        while(true){
+            String token = lexer.next_token();
+            if(lexer.yyatEOF()){
+                break;
+            }
+        }
+    }
+}
---
+++ src/phase1/lexFile.jflex
@@ -0,0 +1,23 @@
+
+
+%%
+
+%class Lexer
+%unicode
+%type String
+%function next_token
+%line
+%column
+
+
+%%
+
+<YYINITIAL>{
+    {}{
+
+    }
+
+    ""{
+
+    }
+}
