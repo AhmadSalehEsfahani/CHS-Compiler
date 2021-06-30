c2636326c34ee1644fcc5178077697d714111e3b
Ahmad Saleh <asalehesfahani@gmail.com>
2021-07-01 00:44:16 +0430

    float has added to register pool

--- src/codeGenerator/RegisterPool.java
+++ src/codeGenerator/RegisterPool.java
@@ -9,6 +9,7 @@ public class RegisterPool {
     private static Queue<String> savedTemporaryRegisters = new LinkedList<>();
     private static Queue<String> argumentRegisters = new LinkedList<>();
     private static Queue<String> funcResultRegisters = new LinkedList<>();
+    private static Queue<String> floatRegisters = new LinkedList<>();
 
     static {
         temporaryRegisters.addAll(Arrays.asList("$t0", "$t1", "$t2", "$t3",
@@ -17,6 +18,11 @@ public class RegisterPool {
                                                         , "$s6", "$s7"));
         argumentRegisters.addAll(Arrays.asList("$a0", "$a1", "$a2", "$a3"));
         funcResultRegisters.addAll(Arrays.asList("$v0", "$v1"));
+        floatRegisters.addAll(Arrays.asList("$f0", "$f1", "$f2", "$f3", "$f4", "$f5", "$f6", "$f7"
+                                                , "$f8", "$f9", "$f10", "$f11", "$f12", "$f13"
+                                                , "$f14", "$f15", "$f16", "$f17", "$f18", "$f19"
+                                                , "$f20", "$f21", "$f22", "$f23", "$f24", "$f25"
+                                                , "$f26", "$f27", "$f28", "$f29", "$f30", "$f31"));
     }
 
     public static String getSP(){
@@ -47,6 +53,10 @@ public class RegisterPool {
         return funcResultRegisters.remove();
     }
 
+    public static String getFloat(){
+        return floatRegisters.remove();
+    }
+
     public static void backTemp(String reg){
         temporaryRegisters.add(reg);
     }
@@ -62,5 +72,11 @@ public class RegisterPool {
     public static void backFuncRes(String reg){
         funcResultRegisters.add(reg);
     }
+
+    public static void backFloat(String reg){
+        floatRegisters.add(reg);
+    }
+
+
 
 }
