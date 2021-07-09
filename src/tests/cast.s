f45de4511e764cbdb20f6659e8a01f3b26afbe73
Ahmad Saleh <asalehesfahani@gmail.com>
2021-07-10 01:43:20 +0430

    tests added

---
+++ src/tests/cast.s
@@ -0,0 +1,17 @@
+class Main {
+    let i : int;
+    let j : real;
+    let n : int;
+    let list : int[];
+
+    main () : int {
+
+
+        n <- int (2.24);
+        out_int(n);
+        out_string(" ");
+        j <- real (5);
+        out_real(j);
+
+    }
+}
